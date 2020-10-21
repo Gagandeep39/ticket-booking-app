9d7907e8dc7ac614a60df151ce52c7dfa3ca788e
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-10-22 02:01:10 +0530

    Implemented Current User route

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -6,8 +6,19 @@
  * @desc Manage routes for CUrrently logged in user
  */
 import express from 'express';
+import jwt from 'jsonwebtoken';
 const router = express.Router();
 
-router.get('/api/users/currentuser', (req, res) => res.send('Current User'));
+router.get('/api/users/currentuser', (req, res, next) => {
+  if (!req.session || !req.session.jwt) return res.send({ currentUser: null });
+  if (!process.env.JWT_KEY) throw new Error('JWT Secret key Not found');
+
+  try {
+    const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY);
+    res.send({ currentUser: payload });
+  } catch (error) {
+    res.send({ currentUser: null });
+  }
+});
 
 export { router as currentUserRouter };
