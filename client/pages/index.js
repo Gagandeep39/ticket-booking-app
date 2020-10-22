208748a0281b51cc560dead0a3d16a6a4d5d4bc2
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-10-22 14:44:41 +0530

    client - Created landing path

---
+++ client/pages/index.js
@@ -0,0 +1,8 @@
+/**
+ * @author Gagandeep Singh
+ * @email singh.gagandeep3911@gmail.com
+ * @create date 2020-10-22 14:16:03
+ * @modify date 2020-10-22 14:16:03
+ * @desc Starting point
+ */
+export default () => (<h1>Landing Page</h1>)
