61e185ea5a75f0d2946767e0ca57d2b429dfc129
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-10-22 20:05:29 +0530

    client - Added comment for understanding

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 /**
  * @author Gagandeep Singh
  * @email singh.gagandeep3911@gmail.com
@@ -5,4 +7,27 @@
  * @modify date 2020-10-22 14:16:03
  * @desc Starting point
  */
-export default () => (<h1>Landing Page</h1>)
+const LandingPage = ({ color }) => {
+  // console.log(color);
+  // axios.get('/api/users/currentUser').then((response) => {
+  //   console.log(response.data);
+  // });
+
+  return <h1>Landing Page</h1>;
+};
+
+/**
+ * Axios code will fail in below code
+ * 1. Local host is the Gateway IP of kubernetes
+ * 2. NextJS runs inside Kubernetes
+ * 3. Different services have dynamic IPs in it
+ * 4. When rquest is made from outside Kubernetes, proper mapping is performed by gateway
+ * 5. When request is made from inside, no interaction is done with gateway, the client doesn;t know who must recieve this request
+ */
+LandingPage.getInitialProps = async () => {
+  // Endsup with error
+  // const response = await axios.get('/api/users/currentUser');
+  return { color: 'red' };
+};
+
+export default LandingPage;
