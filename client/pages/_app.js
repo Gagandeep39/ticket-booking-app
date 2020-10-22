81eb19a9e04f3437aed4c3bc422126ae3d8d6204
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-10-22 17:30:05 +0530

    client - Created global CSS file for bootstrap

---
+++ client/pages/_app.js
@@ -0,0 +1,10 @@
+/**
+ * @author Gagandeep Singh
+ * @email singh.gagandeep3911@gmail.com
+ * @create date 2020-10-22 17:10:07
+ * @modify date 2020-10-22 17:10:07
+ * @desc Standardway to provide Global CSS
+ */
+import 'bootstrap/dist/css/bootstrap.css';
+
+export default ({ Component, pageProps }) => <Component {...pageProps} />;
