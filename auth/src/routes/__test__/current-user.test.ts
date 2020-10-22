05543ccd17d96865bca9f36df9e89d50baef7261
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-10-22 13:50:27 +0530

    Created current user test

---
+++ auth/src/routes/__test__/current-user.test.ts
@@ -0,0 +1,29 @@
+/**
+ * @author Gagandeep Singh
+ * @email singh.gagandeep3911@gmail.com
+ * @create date 2020-10-22 13:41:58
+ * @modify date 2020-10-22 13:41:58
+ * @desc Test current user route
+ */
+
+import request from 'supertest';
+import { app } from '../../app';
+
+/**
+ * Respond with details of current user
+ */
+it('Respond with details of current user', async () => {
+  const res = await request(app)
+    .post('/api/users/signup')
+    .send({ email: 'singh.gagandeep3911@gmail.com', password: '123456' });
+  const cookie = res.get('Set-Cookie');
+
+  await request(app)
+    .get('/api/users/currentuser')
+    .set('Cookie', cookie)
+    .send()
+    .then((response) => {
+      console.log(response.body);
+      expect(response.status).toEqual(200);
+    });
+});
