0a2ac87331055c8448debd42d03a33898493a115
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-10-22 12:56:21 +0530

    Added more test cases

--- auth/src/routes/__test__/sign-up.test.ts
+++ auth/src/routes/__test__/sign-up.test.ts
@@ -8,8 +8,42 @@
 import request from 'supertest';
 import { app } from '../../app';
 
+/**
+ * Test whether user creation returns @code 201
+ */
 it('Returns 201 on Successful sign Up', () =>
   request(app)
     .post('/api/users/signup')
     .send({ email: 'singh.gagandeep3911@gmal.com', password: '123456' })
     .expect(201));
+
+/**
+ *Retuns @code 400 on invalid email
+ */
+it('Returns 400 with invalid data', () => {
+  // Invalid Email
+  request(app)
+    .post('/api/users/signup')
+    .send({ email: 'singh.gagandeep3911gmal.com', password: '123456' })
+    .expect(200);
+
+  // No password
+  request(app)
+    .post('/api/users/signup')
+    .send({ email: 'singh.gagandeep3911gmal.com' })
+    .expect(200);
+});
+
+/**
+ * Prevent duplicate email
+ */
+it('Prevent signup with invalid email', () => {
+  request(app)
+    .post('/api/users/signup')
+    .send({ email: 'singh.gagandeep3911@gmal.com', password: '123456' })
+    .expect(201);
+  request(app)
+    .post('/api/users/signup')
+    .send({ email: 'singh.gagandeep3911@gmal.com', password: '123456' })
+    .expect(400);
+});
