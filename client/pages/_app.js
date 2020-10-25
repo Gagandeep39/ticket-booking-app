cc64168bc97257e15d880f2a64d1b00c065e28ea
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-10-25 16:12:34 +0530

    client - Added a header in client

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -7,11 +7,12 @@
  */
 import 'bootstrap/dist/css/bootstrap.css';
 import buildClient from '../api/build-client';
+import Header from '../components/header';
 
 const AppComponent = ({ Component, pageProps, currentUser }) => {
   return (
     <div>
-      <h1>Hello {currentUser?.email} </h1>
+      <Header />
       <Component {...pageProps} />
     </div>
   );
