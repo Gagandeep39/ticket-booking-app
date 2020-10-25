e67f12cecfc354f21f6be87683b0a730d59272c0
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-10-25 16:45:27 +0530

    tickets - Added ticket validator

--- tickets/src/routes/create-ticket.ts
+++ tickets/src/routes/create-ticket.ts
@@ -5,14 +5,18 @@
  * @modify date 2020-10-25 12:02:21
  * @desc Create ticket routes
  */
-import { requireAuth } from '@gagan-personal/common';
+import { requireAuth, validateRequest } from '@gagan-personal/common';
 import express, { NextFunction, Request, Response } from 'express';
+import { createTicketsValidator } from '../validators/create-tickets';
 const router = express.Router();
 
 router.post(
   '/api/tickets',
   requireAuth,
+  createTicketsValidator,
+  validateRequest,
   (req: Request, res: Response, next: NextFunction) => {
+
     res.set({ msg: 'Response from POST /api/tickets' });
   }
 );
---
+++ tickets/src/validators/create-tickets.ts
@@ -0,0 +1,20 @@
+/**
+ * @author Gagandeep Singh
+ * @email singh.gagandeep3911@gmail.com
+ * @create date 2020-10-25 16:24:58
+ * @modify date 2020-10-25 16:24:58
+ * @desc Perform Tickets Validation
+ */
+import { body } from 'express-validator';
+
+export const createTicketsValidator = [
+  body('title')
+    .not()
+    .isEmpty()
+    .withMessage('Title Cannot be empty'),
+  body('price')
+    .isEmpty()
+    .withMessage('Price cannot be Empty')
+    .isFloat({gt: 0})
+    .withMessage('Price must be greater than 0')
+];
