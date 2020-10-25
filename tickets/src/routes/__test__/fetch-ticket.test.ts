60ff43e7d217565ed28220a1da9d0284d0fc5fdd
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-10-25 20:02:55 +0530

    tickets - Fixed test case for fetch ticket

--- tickets/src/routes/__test__/fetch-ticket.test.ts
+++ tickets/src/routes/__test__/fetch-ticket.test.ts
@@ -5,37 +5,39 @@
  * @modify date 2020-10-25 17:07:51
  * @desc Tests cases for fetch Ticket route
  */
+import mongoose from 'mongoose';
 import request from 'supertest';
 import { app } from '../../app';
 
 it('Return 404 if ticket not found', async () => {
-  request(app)
-    .get('/api/tickets/sdcfvgbbf')
+  const id = new mongoose.Types.ObjectId().toHexString();
+  await request(app)
+    .get(`/api/tickets/${id}`)
     .send()
     .then((response) => {
+      console.log(response.status);
+      
       expect(response.status).toEqual(404);
     });
 });
+
 it('Return Ticket if Found', async () => {
-  const title = 'Ticket';
+  const title = 'Ticketass';
   const price = 99;
 
-  request(app)
+  const response = await request(app)
     .post('/api/tickets')
     .set('Cookie', global.signIn())
     .send({
       title,
       price,
-    })
+    });
+  await request(app)
+    .get(`/api/tickets/${response.body.id}`)
+    .send()
     .then((response) => {
-      expect(response.status).toEqual(201);
-      request(app)
-        .get(`/api/tickets/${response.body.id}`)
-        .send()
-        .then((response) => {
-          expect(response.status).toEqual(200);
-          expect(response.body.title).toEqual(title);
-          expect(response.body.price).toEqual(price);
-        });
+      expect(response.status).toEqual(200);
+      expect(response.body.title).toEqual(title);
+      expect(response.body.price).toEqual(price);
     });
 });
