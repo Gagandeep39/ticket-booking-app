16f068c6c9e902a7aaa2a4f7894fcd83df74dd48
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-12-03 11:35:04 +0530

    Added an event in publisher

--- nats-demo/src/publisher.ts
+++ nats-demo/src/publisher.ts
@@ -8,5 +8,17 @@
 import nats from 'node-nats-streaming';
 require('dotenv').config();
 
-const stan = nats.connect('ticketing', 'abc', { url: `http://${process.env.NATS_URL}:4222` });
-stan.on('connect', () => console.log('Publisher connected to NATS'));
+const stan = nats.connect('ticketing', 'abc', {
+  url: `http://${process.env.NATS_URL}:4222`,
+});
+stan.on('connect', () => {
+  console.log('Publisher connected to NATS');
+  const data = JSON.stringify({
+    id: '123',
+    title: 'concert',
+    price: 20,
+  });
+
+  stan.publish('ticket:created', data, () => console.log('Ticket Created: Event Published')
+  )
+});
