529ad3fd8907e4bbcfac066094dddeffc84ec356
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-12-03 11:51:17 +0530

    Added code to read message

--- nats-demo/src/listener.ts
+++ nats-demo/src/listener.ts
@@ -5,7 +5,8 @@
  * @modify date 2020-12-03 11:37:07
  * @desc Listener event
  */
-import nats from 'node-nats-streaming';
+import nats, { Message } from 'node-nats-streaming';
+import { type } from 'os';
 require('dotenv').config();
 
 const stan = nats.connect('ticketing', 'client-listener', {
@@ -15,5 +16,11 @@ stan.on('connect', () => {
   console.log('Listener connected to NATS');
   const subscription = stan.subscribe('ticket:created');
 
-  subscription.on('message', (msg) => console.log('Message recieved.'));
+  subscription.on('message', (msg: Message) => {
+    console.log('-------------------');
+    console.log('Message recieved.');
+    console.log(`Recieved event #${msg.getSubject()}`);
+    console.log(`Recieved Data #${msg.getData()}`);
+    console.log('-------------------');
+  });
 });
