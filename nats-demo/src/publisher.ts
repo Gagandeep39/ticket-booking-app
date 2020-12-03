0a6d11afd981bed27bb5b9df66d0490960ae8d2d
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-12-03 11:30:09 +0530

    Created a basic publisher

---
+++ nats-demo/src/publisher.ts
@@ -0,0 +1,12 @@
+/**
+ * @author Gagandeep Singh
+ * @email singh.gagandeep3911@gmail.com
+ * @create date 2020-12-02 18:40:16
+ * @modify date 2020-12-02 18:40:16
+ * @desc [description]
+ */
+import nats from 'node-nats-streaming';
+require('dotenv').config();
+
+const stan = nats.connect('ticketing', 'abc', { url: `http://${process.env.NATS_URL}:4222` });
+stan.on('connect', () => console.log('Publisher connected to NATS'));
