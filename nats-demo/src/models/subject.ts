bc1489e5229730397b1f2cb6486b5fe300889f44
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-12-03 18:37:23 +0530

    Created event related models

---
+++ nats-demo/src/models/subject.ts
@@ -0,0 +1,11 @@
+/**
+ * @author Gagandeep Singh
+ * @email singh.gagandeep3911@gmail.com
+ * @create date 2020-12-03 18:15:45
+ * @modify date 2020-12-03 18:15:45
+ * @desc Lists all events available in our event lsitener
+ */
+export enum Subject {
+  TicketCreated = 'ticket:created',
+  OrderUpdated = 'order:updated',
+}
