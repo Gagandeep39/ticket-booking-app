3ae5f7d579de08527fff2a5939a4c8abcbe0e2ca
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-12-04 14:58:30 +0530

    Moved nats connection code to config

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -9,22 +9,12 @@ import dotenv from 'dotenv';
 
 import { connectDB } from './config/db';
 import { app } from './app';
-import { natsWrapper } from './config/nats-wrapper';
+import { connectNAT } from './config/nats';
 
 dotenv.config();
-//  NATS related Events
-natsWrapper
-  .connect('ticketing', 'asxds', `http://${process.env.NATS_URI || 'localhost'}:4222`)
-  .catch((error) => {
-    throw new Error('Error Connecting to NAT');
-  });
-natsWrapper.client.on('close', () => {
-  console.log('NATS Connection Closed');
-  process.exit();
-});
-process.on('SIGINT', () => natsWrapper.client.close());
-process.on('SIGTERM', () => natsWrapper.client.close());
 
+//  NATS related Events
+connectNAT();
 //  Mongo DB Connection
 connectDB();
 
