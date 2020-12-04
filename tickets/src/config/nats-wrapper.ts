b3c2b6b3f9b4097b79959c21544ddb3d8ee3b3d2
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-12-04 11:56:48 +0530

    Created a nats connection class

---
+++ tickets/src/config/nats-wrapper.ts
@@ -0,0 +1,29 @@
+/**
+ * @author Gagandeep Singh
+ * @email singh.gagandeep3911@gmail.com
+ * @create date 2020-12-04 11:16:18
+ * @modify date 2020-12-04 11:16:18
+ * @desc NATS connection using singleton implementation
+ */
+import nats, { Stan } from 'node-nats-streaming';
+class NatsWrapper {
+  // ? implies it may be undefined sometimes
+  private _client?: Stan;
+
+  connect(clusterId: string, clientId: string, url: string) {
+    this._client = nats.connect(clusterId, clientId, { url: url });
+    return new Promise((resolve, reject) => {
+      // ! implies, we know its not null
+      this._client!.on('connect', () => {
+        console.log('NATS Server connected...');
+        console.log(`ClusterID: ${clusterId} | ClientID: ${clientId}`);
+        resolve();
+      });
+      this._client!.on('error', (error) => {
+        reject(error);
+      });
+    });
+  }
+}
+
+export const natsWrapper = new NatsWrapper();
