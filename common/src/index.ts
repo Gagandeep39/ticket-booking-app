753ecc26f595a25e1a9534f6ed0a4cc4c803798e
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-12-07 19:07:59 +0530

    Fixed a typo in common module

--- common/src/index.ts
+++ common/src/index.ts
@@ -39,4 +39,4 @@ export * from './events/custom-publisher';
 export * from './events/ticket-created-event';
 export * from './events/ticket-updated-event';
 export * from './events/order-created-event';
-export * from './events/order-created-event';
+export * from './events/order-cancelled-event';
