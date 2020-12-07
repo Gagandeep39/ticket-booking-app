723e34e886a5fcc52e057385ad8f451113401f40
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-12-07 17:27:42 +0530

    Added more test cases

--- orders/src/routes/__test__/create-order.test.ts
+++ orders/src/routes/__test__/create-order.test.ts
@@ -8,6 +8,9 @@
 import { app } from '../../app';
 import request from 'supertest';
 import mongoose from 'mongoose';
+import { Ticket } from '../../models/ticket';
+import { Order } from '../../models/order';
+import { OrderStatus } from '@gagan-personal/common';
 
 it('Retunrs Error if ticket doesnt exist', async () => {
   const ticketId = mongoose.Types.ObjectId();
@@ -20,6 +23,43 @@ it('Retunrs Error if ticket doesnt exist', async () => {
     });
 });
 
-it('Retunrs Error if ticket reserved', async () => {});
+it('Retunrs Error if ticket reserved', async () => {
+  // Insert tick and create order
+  const ticket = Ticket.build({
+    price: 20,
+    title: 'Dummy',
+  });
+  await ticket.save();
+  const order = Order.build({
+    expiresAt: new Date(),
+    status: OrderStatus.Created,
+    ticket,
+    userId: 'sxdcfvgbh',
+  });
 
-it('Reserve a ticket', async () => {});
+  await order.save();
+  // Reinsert to see if reserved
+  await request(app)
+    .post('/api/orders')
+    .set('Cookie', global.signIn())
+    .send({ ticketId: ticket.id })
+    .then((response) => {
+      expect(response.status).toEqual(400);
+    });
+});
+
+it('Reserve a ticket', async () => {
+  // Insert tick and create order
+  const ticket = Ticket.build({
+    price: 20,
+    title: 'Dummy',
+  });
+  await ticket.save();
+  await request(app)
+    .post('/api/orders')
+    .set('Cookie', global.signIn())
+    .send({ ticketId: ticket.id })
+    .then((response) => {
+      expect(response.status).toEqual(201);
+    });
+});
