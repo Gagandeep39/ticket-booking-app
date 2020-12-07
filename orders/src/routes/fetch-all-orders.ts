d9b9dbaf5c56166a30c4c7b1c34f7bdeb539c662
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-12-07 17:38:04 +0530

    Added fetch all logic

--- orders/src/routes/fetch-all-orders.ts
+++ orders/src/routes/fetch-all-orders.ts
@@ -7,12 +7,18 @@
  */
 import { requireAuth } from '@gagan-personal/common';
 import express, { NextFunction, Request, Response } from 'express';
+import { Order } from '../models/order';
 const router = express.Router();
 
 router.get(
-  '/api/tickets',
+  '/api/orders',
   requireAuth,
-  (req: Request, res: Response, next: NextFunction) => {}
+  (req: Request, res: Response, next: NextFunction) => {
+    Order.find({ userId: req.currentUser!.id })
+      .populate('ticket') // Fetch ticket for each order
+      .then((orders) => res.send(orders))
+      .catch((error) => next(error));
+  }
 );
 
 export { router as fetchAllOrdersRouter };
