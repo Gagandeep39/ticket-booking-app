c6a605fb5fe846fc9f781af25531f82f4aefe816
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-12-28 20:17:55 +0530

    Added a test case to test error in update ticket

--- tickets/src/routes/__test__/update-ticket.test.ts
+++ tickets/src/routes/__test__/update-ticket.test.ts
@@ -9,6 +9,7 @@ import mongoose from 'mongoose';
 import request from 'supertest';
 import { app } from '../../app';
 import { natsWrapper } from '../../config/nats-wrapper';
+import { Ticket } from '../../models/tickets';
 
 it('Return 404, if the provideed ID doesnt exist', async () => {
   const id = new mongoose.Types.ObjectId().toHexString();
@@ -121,4 +122,35 @@ it('Publishes an Event', async () => {
       expect(response.status).toEqual(200);
     });
   expect(natsWrapper.client.publish).toHaveBeenCalled();
+});
+
+it('Reject Update if ticket is reserved', async () => {
+  // Sign in
+  const cookie = global.signIn();
+  // Create a ticket
+  const response = await request(app)
+    .post('/api/tickets')
+    .set('Cookie', cookie)
+    .send({
+      title: 'Ticket',
+      price: 99,
+    });
+
+  // Add orderId to ticket to test fail
+  const ticket = await Ticket.findById(response.body.id);
+  await ticket
+    ?.set({ orderId: mongoose.Types.ObjectId().toHexString() })
+    .save();
+  // Try to update a ticket
+  await request(app)
+    .put(`/api/tickets/${response.body.id}`)
+    .set('Cookie', cookie)
+    .send({
+      title: 'Correct title',
+      price: 999,
+    })
+    .then((response) => {
+      // Must fail as we cant update a locked ticket (Ticket associated with order ID)
+      expect(response.status).toEqual(400);
+    });
 });
