1f46dba90ea161dcb1f97084144d760ed24370cb
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-12-29 12:52:36 +0530

    Fixed typo

--- expiration/src/events/listeners/order-created-listener.ts
+++ expiration/src/events/listeners/order-created-listener.ts
@@ -20,7 +20,7 @@ export class OrderCreatedListener extends CustomListener<OrderCreatedEvent> {
 
   async onMessage(data: OrderCreatedEvent['data'], msg: Message) {
     const delay = new Date(data.expiresAt).getTime() - new Date().getTime();
-    console.log('Waiting duration for procssing the job', delay);
+    console.log('Waiting duration for processing the job: ', delay);
 
     await expirationQueue.add({ orderId: data.id }, { delay });
     msg.ack();
