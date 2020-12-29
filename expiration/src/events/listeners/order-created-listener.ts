ddba9bd85abd5cd0fc6ac8c687025707b7e08caa
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-12-29 11:57:21 +0530

    Created order created listener

---
+++ expiration/src/events/listeners/order-created-listener.ts
@@ -0,0 +1,27 @@
+/**
+ * @author Gagandeep Singh
+ * @email singh.gagandeep3911@gmail.com
+ * @create date 2020-12-29 10:52:59
+ * @modify date 2020-12-29 10:52:59
+ * @desc [description]
+ */
+import {
+  CustomListener,
+  OrderCreatedEvent,
+  Subject,
+} from '@gagan-personal/common';
+import { Message } from 'node-nats-streaming';
+import { queueGroupName } from '../../constants/queue-group-names';
+import { expirationQueue } from '../../queues/expiration-queue';
+
+export class OrderCreatedListener extends CustomListener<OrderCreatedEvent> {
+  subject: Subject.OrderCreated = Subject.OrderCreated;
+  queueGroupName: string = queueGroupName;
+
+  async onMessage(data: OrderCreatedEvent['data'], msg: Message) {
+    await expirationQueue.add({
+      orderId: data.id,
+    });
+    msg.ack();
+  }
+}
