a029aa347cec264785585a3995548f480c62101e
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-12-29 11:57:34 +0530

    Created bull queue

---
+++ expiration/src/models/payload.ts
@@ -0,0 +1,10 @@
+/**
+ * @author Gagandeep Singh
+ * @email singh.gagandeep3911@gmail.com
+ * @create date 2020-12-29 11:11:23
+ * @modify date 2020-12-29 11:11:23
+ * @desc [description]
+ */
+export interface Payload {
+  orderId: string;
+}
---
+++ expiration/src/queues/expiration-queue.ts
@@ -0,0 +1,23 @@
+/**
+ * @author Gagandeep Singh
+ * @email singh.gagandeep3911@gmail.com
+ * @create date 2020-12-29 11:05:21
+ * @modify date 2020-12-29 11:05:21
+ * @desc [description]
+ */
+import Queue from 'bull';
+import { Payload } from '../models/payload';
+
+const expirationQueue = new Queue<Payload>('order:expiration', {
+  redis: {
+    host: process.env.REDIS_HOST,
+  },
+});
+
+expirationQueue.process(async (job) => {
+  console.log('Publishing an expiration:complete event', job.data.orderId);
+
+  console.log(job);
+});
+
+export { expirationQueue };
