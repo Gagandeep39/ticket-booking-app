ad5ebd0b6053c2d1d79d67d80d2abe27cf0de778
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-12-30 19:54:32 +0530

    Updated test case

--- payments/src/routes/__test__/create-payment.test.ts
+++ payments/src/routes/__test__/create-payment.test.ts
@@ -74,16 +74,51 @@ it('400, if order cancelled', async () => {
     });
 });
 
+// it('Returns 201 with valid inputs', async () => {
+//   const userId = mongoose.Types.ObjectId().toHexString();
+//   // Create order with a random user
+//   const order = await Order.build({
+//     id: mongoose.Types.ObjectId().toHexString(),
+//     userId,
+//     version: 0,
+//     price: 20,
+//     status: OrderStatus.Created,
+//   }).save();
+
+//   await request(app)
+//     .post('/api/payments')
+//     .set('Cookie', global.signIn(userId))
+//     .send({
+//       token: 'tok_visa',
+//       orderId: order.id,
+//     })
+//     .then((response) => {
+//       expect(response.status).toEqual(201);
+//     });
+
+//   const chargeOptions = (stripe.charges.create as jest.Mock).mock.calls[0][0];
+//   expect(chargeOptions.source).toEqual('tok_visa');
+//   expect(chargeOptions.amount).toEqual(20 * 100);
+//   expect(chargeOptions.currency).toEqual('inr');
+// });
+
+// Steps to test against actual API
+// 1. Remove mock file
+// 2. Remove jest statment
+// 3. Add API key in test/setup
+// 4. Call a route handler -> Route will send request to server -> Rsponse from Stripe -> Fetch response inside test -> Compare response with Stripe server test
 it('Returns 201 with valid inputs', async () => {
   const userId = mongoose.Types.ObjectId().toHexString();
+  const price = Math.floor(Math.random() * 100000);
   // Create order with a random user
   const order = await Order.build({
     id: mongoose.Types.ObjectId().toHexString(),
     userId,
     version: 0,
-    price: 20,
+    price,
     status: OrderStatus.Created,
   }).save();
+  console.log(process.env.STRIPE_SECRET_KEY);
 
   await request(app)
     .post('/api/payments')
@@ -93,58 +128,22 @@ it('Returns 201 with valid inputs', async () => {
       orderId: order.id,
     })
     .then((response) => {
+      console.log(response.status);
+
       expect(response.status).toEqual(201);
     });
 
-  const chargeOptions = (stripe.charges.create as jest.Mock).mock.calls[0][0];
-  expect(chargeOptions.source).toEqual('tok_visa');
-  expect(chargeOptions.amount).toEqual(20 * 100);
-  expect(chargeOptions.currency).toEqual('inr');
+  const stripeCharges = await stripe.charges.list({
+    limit: 50,
+  });
+  const stripeCharge = stripeCharges.data.find(
+    (charge) => charge.amount === price * 100
+  );
+  expect(stripeCharge).toBeDefined();
+  expect(stripeCharge?.currency).toEqual('inr');
+  const payment = Payment.findOne({
+    orderId: order.id,
+    stripeId: stripeCharge?.id,
+  });
+  expect(payment).not.toBeNull();
 });
-
-// Steps to test against actual API
-// 1. Remove mock file
-// 2. Remove jest statment
-// 3. Add API key in test/setup
-// 4. Call a route handler -> Route will send request to server -> Rsponse from Stripe -> Fetch response inside test -> Compare response with Stripe server test
-// it('Returns 201 with valid inputs', async () => {
-//   const userId = mongoose.Types.ObjectId().toHexString();
-//   const price = Math.floor(Math.random() * 100000);
-//   // Create order with a random user
-//   const order = await Order.build({
-//     id: mongoose.Types.ObjectId().toHexString(),
-//     userId,
-//     version: 0,
-//     price,
-//     status: OrderStatus.Created,
-//   }).save();
-//   console.log(process.env.STRIPE_SECRET_KEY);
-
-//   await request(app)
-//     .post('/api/payments')
-//     .set('Cookie', global.signIn(userId))
-//     .set({ Authorization: `Bearer ${process.env.STRIPE_SECRET_KEY}` })
-//     .send({
-//       token: 'tok_visa',
-//       orderId: order.id,
-//     })
-//     .then((response) => {
-//       console.log(response.status);
-
-//       expect(response.status).toEqual(201);
-//     });
-
-//   const stripeCharges = await stripe.charges.list({
-//     limit: 50,
-//   });
-//   const stripeCharge = stripeCharges.data.find(
-//     (charge) => charge.amount === price * 100
-//   );
-//   expect(stripeCharge).toBeDefined();
-//   expect(stripeChange?.currency).toEqual('inr');
-// const payment = Payment.findOne({
-//   orderId: order.id,
-//   stripeId: stripeChange?.id,
-// });
-// expect(payment).not.toBeNull();
-// });
