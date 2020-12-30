2ca6a012d12bd726b2072245d32857efe92aa30c
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-12-30 13:20:37 +0530

    Added error validation in create payment

--- payments/src/routes/create-payment.ts
+++ payments/src/routes/create-payment.ts
@@ -5,8 +5,16 @@
  * @modify date 2020-12-30 12:35:29
  * @desc Create payment
  */
-import { requireAuth, validateRequest } from '@gagan-personal/common';
+import {
+  BadRequestError,
+  NotAuthorizedError,
+  NotFoundError,
+  OrderStatus,
+  requireAuth,
+  validateRequest,
+} from '@gagan-personal/common';
 import express, { NextFunction, Request, Response } from 'express';
+import { Order } from '../models/order';
 import { createPaymentsValidator } from '../validators/create-payment';
 const router = express.Router();
 
@@ -15,7 +23,15 @@ router.post(
   requireAuth,
   createPaymentsValidator,
   validateRequest,
-  (req: Request, res: Response, next: NextFunction) => {
+  async (req: Request, res: Response, next: NextFunction) => {
+    const { token, orderId } = req.body;
+    const order = await Order.findById(orderId);
+
+    if (!order) throw new NotFoundError();
+    if (order.userId !== req.currentUser?.id) throw new NotAuthorizedError();
+    if (order.status === OrderStatus.Cancelled)
+      throw new BadRequestError('Order already cancelled');
+
     res.send('Success');
   }
 );
