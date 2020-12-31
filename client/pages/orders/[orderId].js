46d12b071c38a1621ddb78d439a4ca846f6dabe3
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-12-31 16:59:35 +0530

    Added order screen

---
+++ client/pages/orders/[orderId].js
@@ -0,0 +1,35 @@
+/**
+ * @author Gagandeep Singh
+ * @email singh.gagandeep3911@gmail.com
+ * @create date 2020-12-31 16:16:20
+ * @modify date 2020-12-31 16:16:20
+ * @desc [description]
+ */
+
+import React, { useEffect, useState } from 'react';
+
+const OrderShow = ({ order }) => {
+  const [timeLeft, setTimeLeft] = useState('');
+
+  useEffect(() => {
+    const findTimeLeft = () => {
+      const msLeft = new Date(order.expiresAt) - new Date();
+      setTimeLeft(Math.round(msLeft / 1000));
+    };
+
+    findTimeLeft();
+    const timeId = setInterval(findTimeLeft, 1000);
+    return () => {
+      clearInterval(timeId);
+    };
+  }, [order]);
+  return <div>{timeLeft} seconds until order expires</div>;
+};
+
+OrderShow.getInitialProps = async (context, client) => {
+  const { orderId } = context.query;
+  const { data } = await client.get(`/api/orders/${orderId}`);
+  return { order: data };
+};
+
+export default OrderShow;
