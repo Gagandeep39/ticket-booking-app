e22bc6290a7ea3577814436c2fce5c6f2b3973c9
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-12-31 16:58:41 +0530

    Created purcahse ticket screen

--- client/pages/tickets/[ticketId].js
+++ client/pages/tickets/[ticketId].js
@@ -1,7 +1,34 @@
 import React from 'react';
+import useRequest from '../../hooks/use-request';
+import Router from 'next/router';
 
-const TickedShow = () => {
-  return <div></div>;
+const TicketShow = ({ ticket }) => {
+  const { doRequest, errors } = useRequest({
+    url: '/api/orders',
+    method: 'post',
+    body: {
+      ticketId: ticket.id,
+    },
+    onSuccess: (order) =>
+      Router.push('/orders/[orderId]', `/orders/${order.id}`),
+  });
+
+  return (
+    <div>
+      <h1> {ticket.title} </h1>
+      <h4> Price: ₹{ticket.price} </h4>
+      {errors}
+      <button className="btn btn-primary" onClick={doRequest}>
+        Purchase
+      </button>
+    </div>
+  );
 };
 
-export default TickedShow;
+TicketShow.getInitialProps = async (context, client) => {
+  const { ticketId } = context.query;
+  const { data } = await client.get(`/api/tickets/${ticketId}`);
+  return { ticket: data };
+};
+
+export default TicketShow;
