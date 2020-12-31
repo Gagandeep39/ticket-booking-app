9fe15837337272a20db633099da51fb030115552
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-12-31 12:43:59 +0530

    Added server logic

--- client/pages/tickets/new.js
+++ client/pages/tickets/new.js
@@ -7,10 +7,17 @@
  */
 
 import React, { useState } from 'react';
+import useRequest from '../../hooks/use-request';
 
 const NewTicket = () => {
   const [title, setTitle] = useState('');
   const [price, setPrice] = useState('');
+  const { doRequest, errors } = useRequest({
+    url: '/api/tickets',
+    method: 'post',
+    body: { title, price },
+    onSuccess: (ticket) => console.log(ticket),
+  });
 
   const onBlur = () => {
     const value = parseFloat(price);
@@ -20,10 +27,15 @@ const NewTicket = () => {
     setPrice(value.toFixed(2));
   };
 
+  const onSubmit = (event) => {
+    event.preventDefault();
+    doRequest();
+  };
+
   return (
     <div>
       <h1>Create a ticket</h1>
-      <form>
+      <form onSubmit={onSubmit}>
         <div className="form-group">
           <label>Title</label>
           <input
@@ -43,6 +55,7 @@ const NewTicket = () => {
             className="form-control"
           />
         </div>
+        {errors}
         <button className="btn btn-primary">Submit</button>
       </form>
     </div>
