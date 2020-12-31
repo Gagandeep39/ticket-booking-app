e5b48c2431423c22354d864551dbfb1a723f508d
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-12-31 18:17:42 +0530

    Fixed doRequest call logic

--- client/pages/tickets/[ticketId].js
+++ client/pages/tickets/[ticketId].js
@@ -18,7 +18,7 @@ const TicketShow = ({ ticket }) => {
       <h1> {ticket.title} </h1>
       <h4> Price: ₹{ticket.price} </h4>
       {errors}
-      <button className="btn btn-primary" onClick={doRequest}>
+      <button className="btn btn-primary" onClick={() => doRequest()}>
         Purchase
       </button>
     </div>
